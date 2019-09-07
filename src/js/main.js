548cfae1846725457ed8ea71441b62a8efe53bf9
igor-kostenevich <kostenevich9696@mail.ru>
2019-09-07 05:31:34 +0300

    Our projects block

---
+++ src/js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  
+  
+    // $('.our-project__carousel').slick({
+    //   slidesToShow: 3,
+    //   slidesToScroll: 1,
+    //   dots: true,
+    //   infinite: true,
+    //   speed: 500,
+    //   fade: true,
+    //   cssEase: 'linear'
+    // });
+});
+
+$(document).ready(function () {
+  
+    $('.our-project__carousel').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+  
+   
+});
